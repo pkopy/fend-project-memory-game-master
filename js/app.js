c90880eeb7ec172425843173df4fa07c2df267db
pkopy <pawel.kopycki@gmail.com>
2018-03-15 11:14:59 +0100

    Add listener for cards

--- js/app.js
+++ js/app.js
@@ -45,3 +45,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const deckOfCards = document.querySelector('.deck');
+deckOfCards.addEventListener('click', function(evt){
+    showSymbol(evt);
+})
+
+function showSymbol(evt){
+    evt.target.classList.add('open', 'show');
+}
