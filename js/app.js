0983368ffc8a365200a308796ab9051bc893ccfa
pkopy <pawel.kopycki@gmail.com>
2018-03-22 09:42:23 +0100

    Format documents

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
  * Create a list that holds all of your cards
  */
 const listOfCards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
-"fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
-"fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
+    "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
+    "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
 
 
 /*
@@ -72,8 +72,8 @@ leaderBoardInit();
  * Listeners
  */
 
-deckOfCards.addEventListener('click', function(evt){
-    if(!(evt.target.className === 'deck') && (openList.length <= 2)&& !(evt.target.isClicked === 1) && !(evt.target.localName === 'i')){
+deckOfCards.addEventListener('click', function (evt) {
+    if (!(evt.target.className === 'deck') && (openList.length <= 2) && !(evt.target.isClicked === 1) && !(evt.target.localName === 'i')) {
         showSymbol(evt);
         addCardToOpenList(evt);
         incrementCounter();
@@ -82,17 +82,16 @@ deckOfCards.addEventListener('click', function(evt){
     }
 })
 
-restartButton.addEventListener('click', function(){
+restartButton.addEventListener('click', function () {
     resetGame();
 })
 
-restartButtonPopup.addEventListener('click', function(){
+restartButtonPopup.addEventListener('click', function () {
     document.querySelector('.win-popup').style.display = "none";
     document.addEventListener('keydown', function pushEnter(evt) {
-        if(evt.keyCode === 13) {
+        if (evt.keyCode === 13) {
             clickNameButton();
             this.removeEventListener('keydown', pushEnter);
-
         }
     })
     openNamePanel();
@@ -110,16 +109,16 @@ submitNameButton.addEventListener('click', clickNameButton);
 
 function clickNameButton() {
     nameOfPlayer = submitNameButton.previousElementSibling.value;
-    if(nameOfPlayer === '') {
+    if (nameOfPlayer === '') {
         nameOfPlayer = 'Joe Doe'
     }
-    addScoreToLeaderBoard({name: nameOfPlayer, score: score, time: endTime/1000, move: counterOfMoves});
+    addScoreToLeaderBoard({ name: nameOfPlayer, score: score, time: endTime / 1000, move: counterOfMoves });
     document.querySelector('.name-panel').style.display = "none";
     increaseOfOpacity(document.querySelector('.win-popup-bg'), 0.7);
     openLeaderBoard();
 }
 
-leaderBoardText.addEventListener('click', function() {
+leaderBoardText.addEventListener('click', function () {
     increaseOfOpacity(document.querySelector('.win-popup-bg'), 0.7);
     openLeaderBoard();
 })
@@ -130,50 +129,50 @@ leaderBoardText.addEventListener('click', function() {
  */
 
 
-function showSymbol(evt){
-    evt.target.className ='card open show';
+function showSymbol(evt) {
+    evt.target.className = 'card open show';
     evt.target.isClicked = 1;
 }
 
-function addCardToOpenList(evt){
-    openList.push(evt.target.firstElementChild); 
+function addCardToOpenList(evt) {
+    openList.push(evt.target.firstElementChild);
     checkTwoCardsMatch(openList);
     checkTwoCardsNotMatch(openList);
 }
 
-function checkTwoCardsMatch(array){
-    if(array.length === 2 && array[0].className === array[1].className){
+function checkTwoCardsMatch(array) {
+    if (array.length === 2 && array[0].className === array[1].className) {
         array[0].parentNode.className = 'card match show';
         array[1].parentNode.className = 'card match show';
         matchList.push(array[0]);
         clearTheOpenList(array);
-    }    
+    }
 }
 
-function checkTwoCardsNotMatch(array){
-    if(array.length === 2 && array[0].className !== array[1].className){
-        setTimeout(function(){
+function checkTwoCardsNotMatch(array) {
+    if (array.length === 2 && array[0].className !== array[1].className) {
+        setTimeout(function () {
             array[0].parentNode.className = 'card close';
             array[1].parentNode.className = 'card close';
             array[0].parentNode.isClicked = 0;
             array[1].parentNode.isClicked = 0;
             clearTheOpenList(array);
-        },800);
+        }, 800);
     }
 }
 
 
-function clearTheOpenList(array){
-    for(let i = 0; i < 2; i++){
+function clearTheOpenList(array) {
+    for (let i = 0; i < 2; i++) {
         array.shift();
     }
-    
+
     return array;
 }
 
-function addRandomSymbolToCard(array){
+function addRandomSymbolToCard(array) {
     let shuffleListOfCards = shuffle(listOfCards);
-    for(i = 0; i < array.length; i++){
+    for (i = 0; i < array.length; i++) {
         array[i].firstElementChild.className = shuffleListOfCards[i];
     }
 }
@@ -182,20 +181,20 @@ function addRandomSymbolToCard(array){
  * Score panel
  */
 
-function removeStarFromScorePanel(){
-    if(counterOfMoves === 30){
-        starsPanel.lastElementChild.style.visibility ='hidden';
-    }else if(counterOfMoves === 50){
-        starsPanel.lastElementChild.previousElementSibling.style.visibility ='hidden';
+function removeStarFromScorePanel() {
+    if (counterOfMoves === 30) {
+        starsPanel.lastElementChild.style.visibility = 'hidden';
+    } else if (counterOfMoves === 50) {
+        starsPanel.lastElementChild.previousElementSibling.style.visibility = 'hidden';
     }
 }
 
-function resetGame(){
-    for(let card of cards){
+function resetGame() {
+    for (let card of cards) {
         card.className = "card close"
         card.isClicked = 0;
     }
-    for(star of stars){
+    for (star of stars) {
         star.style.visibility = 'visible'
     }
     counterOfMoves = 0;
@@ -207,7 +206,7 @@ function resetGame(){
     document.querySelector('.win-popup-bg').style.display = "none";
 }
 
-function incrementCounter(){
+function incrementCounter() {
     counterOfMoves++;
     moveCounterDisplay.innerHTML = counterOfMoves;
 }
@@ -224,16 +223,15 @@ function leaderBoardInit() {
     }
 }
 
-
 function sortObjectInArray(property) {
     return (a, b) => (a[property] < b[property]) ? 1 : (a[property] > b[property]) ? -1 : 0;
 }
-    
+
 function getLeaderBoard() {
     let isIE = /*@cc_on!@*/false || !!document.documentMode;
     let isEdge = !isIE && !!window.StyleMedia;
-    if(!isEdge) {
-        let array =  JSON.parse(localStorage.leaderBoard); 
+    if (!isEdge) {
+        let array = JSON.parse(localStorage.leaderBoard);
         array.sort(sortObjectInArray("score"));
         return array;
     }
@@ -242,7 +240,7 @@ function getLeaderBoard() {
 function addScoreToLeaderBoard(obj) {
     let isIE = /*@cc_on!@*/false || !!document.documentMode;
     let isEdge = !isIE && !!window.StyleMedia;
-    if(!isEdge) {
+    if (!isEdge) {
         let data = JSON.parse(localStorage.leaderBoard);
         data.push(obj);
         localStorage.leaderBoard = JSON.stringify(data)
@@ -257,7 +255,7 @@ function addScoreToLeaderBoard(obj) {
 function openLeaderBoard() {
     let heightElement = 800;
 
-    if (window.innerWidth <= 600 ) {
+    if (window.innerWidth <= 600) {
         heightElement = 500;
     }
     if (window.innerWidth <= 450) {
@@ -267,33 +265,33 @@ function openLeaderBoard() {
     document.querySelector('.win-popup').style.display = "none";
 
     pushTableToLeaderBoard();
-    
 
-    setTimeout(function() {
+
+    setTimeout(function () {
         document.querySelector('.leader-board table').style.display = 'inline-table';
-    },500)
+    }, 500)
 }
 
 function pushTableToLeaderBoard() {
     let scoreBoard = getLeaderBoard();
     let scoreBoardLength = scoreBoard.length;
-    
-    if(scoreBoardLength > 8 && window.innerWidth <= 450) {
+
+    if (scoreBoardLength > 8 && window.innerWidth <= 450) {
         scoreBoardLength = 8;
     }
 
-    if(scoreBoardLength > 12 && window.innerWidth <= 600) {
+    if (scoreBoardLength > 12 && window.innerWidth <= 600) {
         scoreBoardLength = 12;
     }
 
-    if(scoreBoardLength > 23) {
+    if (scoreBoardLength > 23) {
         scoreBoardLength = 23;
     }
 
     for (i = 0; i < scoreBoardLength; i++) {
         let obj = scoreBoard[i];
         const row = document.createElement('tr');
-        for (j = 0; j < Object.keys(obj).length; j++){
+        for (j = 0; j < Object.keys(obj).length; j++) {
             key = Object.keys(obj)[j];
             const col = document.createElement('td');
             col.innerHTML = obj[key];
@@ -306,7 +304,7 @@ function pushTableToLeaderBoard() {
 
 function clearTableofLeaderBoard() {
     let rowsTable = scoreTable.querySelectorAll('tr');
-    for(i = rowsTable.length-1; i > 0; i--) {
+    for (i = rowsTable.length - 1; i > 0; i--) {
         scoreTable.removeChild(rowsTable[i]);
     }
 }
@@ -315,24 +313,24 @@ function clearTableofLeaderBoard() {
  * Render congratulations popup
  */
 
-function changeSizeOfElement(element, widthElement = 400, countH = 10, countW = 20, display){
+function changeSizeOfElement(element, widthElement = 400, countH = 10, countW = 20, display) {
     let countHeight = 0;
     let countWidth = 0;
-    let id = setInterval(function(){
+    let id = setInterval(function () {
         countHeight += countH;
         countWidth += countW;
         element.style.display = display;
         element.style.height = countHeight + 'px';
-        element.style.width = countWidth  + 'px';
-        element.style.opacity = widthElement/(widthElement * 3 - widthElement * 2);
-        if(countHeight >= widthElement){
+        element.style.width = countWidth + 'px';
+        element.style.opacity = widthElement / (widthElement * 3 - widthElement * 2);
+        if (countHeight >= widthElement) {
             clearInterval(id)
         }
     }, 5);
-    
+
 }
 
-function increaseOfOpacity(element, opacity){
+function increaseOfOpacity(element, opacity) {
     let countOpacity = 0
     element.style.opacity = 0;
     element.style.display = 'inherit';
@@ -341,7 +339,7 @@ function increaseOfOpacity(element, opacity){
         element.style.opacity = countOpacity;
         if (countOpacity >= opacity) {
             clearInterval(id)
-        } 
+        }
     }, 10)
 }
 
@@ -356,35 +354,34 @@ function openPopup() {
     if (window.innerWidth <= 450) {
         heightElement = 160;
     }
-    
+
     increaseOfOpacity(document.querySelector('.win-popup-bg'), 0.7);
-    
+
     changeSizeOfElement(document.querySelector('.win-popup'), heightElement, 10, 20, 'inherit');
-    
-    document.querySelector('#score-time').innerHTML = (Math.round(endTime/1000, 2) + ' sec');
-    
+
+    document.querySelector('#score-time').innerHTML = (Math.round(endTime / 1000, 2) + ' sec');
+
     document.querySelector('#score-value').innerHTML = (score + '.00');
-    
+
     if (counterOfMoves <= 29) {
-        document.querySelector('#score-stars').innerHTML = 
-        `<img src="img/star__30x30.png" alt="star">
-        <img src="img/star__30x30.png" alt="Szmebuldog">
-        <img src="img/star__30x30.png" alt="Szmebuldog">`;
+        document.querySelector('#score-stars').innerHTML =
+            `<img src="img/star__30x30.png" alt="star">
+        <img src="img/star__30x30.png" alt="star">
+        <img src="img/star__30x30.png" alt="star">`;
     } else if (counterOfMoves <= 49) {
-        document.querySelector('#score-stars').innerHTML = 
-        `<img src="img/star__30x30.png" alt="star">
-        <img src="img/star__30x30.png" alt="Szmebuldog">`;
+        document.querySelector('#score-stars').innerHTML =
+            `<img src="img/star__30x30.png" alt="star">
+        <img src="img/star__30x30.png" alt="star">`;
     } else {
-        document.querySelector('#score-stars').innerHTML = 
-        `<img src="img/star__30x30.png" alt="star">`
+        document.querySelector('#score-stars').innerHTML =
+            `<img src="img/star__30x30.png" alt="star">`
     }
-    // document.querySelector('.win-popup').style.display = 'inherit';
-    
-    setTimeout(function() {
+
+    setTimeout(function () {
         document.querySelector('table').style.display = 'inline-table';
-    },500)
-    
+    }, 500)
 }
+
 /*
  * Render name panel
  */
@@ -406,19 +403,19 @@ function openNamePanel() {
  * End Game
  */
 
-function timeOfGame(){
-    
+function timeOfGame() {
+
     // Start game
 
-    if(counterOfMoves === 1){
+    if (counterOfMoves === 1) {
         startTime = Date.now();
     }
 
     // End game
 
-    if(matchList.length === 1){
+    if (matchList.length === 8) {
         endTime = Date.now() - startTime;
-        score = (100 - Math.floor(endTime/1000)) + (100- counterOfMoves)
+        score = (100 - Math.floor(endTime / 1000)) + (100 - counterOfMoves)
         openPopup();
     }
 }
